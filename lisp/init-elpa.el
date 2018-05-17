2d104d4f7048aa3f961454842abe6341a6bcee47
adio <liuhuifeng.bj@gmail.com>
2018-05-17 23:13:05 +0800

    update init-elpa.el

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -4,6 +4,22 @@
 (add-to-list 'package-archives '("gnu" . "http://elpa.emacs-china.org/gnu/") t)
 (add-to-list 'package-archives '("melpa" . "http://elpa.emacs-china.org/melpa-stable/") t)
 
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (if (boundp 'package-selected-packages)
+            ;; Record this as a package the user installed explicitly
+            (package-install package nil)
+          (package-install package))
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+
 (defun my/package-install-p(pkg)
   (when (not (package-installed-p pkg))
     (package-refresh-contents)
