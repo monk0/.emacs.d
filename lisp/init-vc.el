d3de202321311b2f556e91135e200e952795a3ee
adio <liuhuifeng.bj@gmail.com>
2018-05-25 14:56:25 +0800

    add vc config

---
+++ lisp/init-vc.el
@@ -0,0 +1,9 @@
+;;; init-vc.el
+
+(when (maybe-require-package 'diff-hl)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+  (add-hook 'after-init-hook 'global-diff-hl-mode))
+
+(maybe-require-package 'browse-at-remote)
+
+(provide 'init-vc)
