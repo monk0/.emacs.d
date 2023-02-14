699739d4e63f41a05276a52298e3575200ab5286
unknown <liuhuifeng7@360buyad.local>
2023-02-14 19:35:20 +0800

    update eglot config

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -5,12 +5,18 @@
 
 ;;; Code:
 
-(use-package eglot-java
+(use-package eglot
   :hook
-  (java-mode-hook . eglot-ensure))
+  (java-mode-hook . eglot-ensure)
+  (xml-mode-hook . eglot-ensure))
 
 ;;(with-eval-after-load 'eglot
-;;  (add-to-list 'eglot-server-programs '(java-mode . ("jdtls"))))
+;;  (add-to-list 'eglot-server-programs '(java-mode . "jdtls")))
+
+(cl-defmethod eglot-execute-command
+  (_server (_cmd (eql java.apply.workspaceEdit)) arguments)
+  "Eclipse JDT breaks spec and replies with edits as arguments."
+  (mapc #'eglot--apply-workspace-edit arguments))
 
 (provide 'init-eglot)
 ;;; init-eglot.el ends here
